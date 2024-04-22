465bdc26940029ab73617159f2528dbb2475efd2
Federico Ahumada <ahufede@gmail.com>
2024-04-22 17:04:11 -0300

    Enemy on target highlight

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -8,6 +8,8 @@
 
 class UInputMappingContext;
 class UInputAction;
+class IEnemyInterface;
+
 struct FInputActionValue;
 
 /**
@@ -19,6 +21,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 	GENERATED_BODY()
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 	
 protected:
 	virtual void BeginPlay() override;
@@ -32,5 +35,8 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
-	
+
+	void CursorTrace();
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };

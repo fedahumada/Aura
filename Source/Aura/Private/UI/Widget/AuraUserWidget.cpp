007417da604565f6cc91c72c95f2d7ae1f7afc97
Federico Ahumada <ahufede@gmail.com>
2024-05-02 20:47:35 -0300

    Game UI - Aura HUD

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Fedahumada 
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
